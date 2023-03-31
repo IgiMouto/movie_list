f65d1a30f7692cd3399f306d1b79d3cd1f48a06b
Luigi Angelo Siguenza <igi.siguenza@gmail.com>
2023-03-31 17:35:23 +0800

    :hammer: Add repository file

--- lib/data/repositories/movies_repository.dart
+++ lib/data/repositories/movies_repository.dart
@@ -1,11 +1,30 @@
-import 'package:flutter/services.dart';
-import 'package:movie_list/data/models/movies_model.dart';
+import 'package:movie_list/data/globals.dart';
 
-class MoviesRepository {
-  Future<MoviesModel> fetchMoviesData() async {
-    dynamic response =
-        await rootBundle.loadString('assets/data/movie_data.json');
+class MovieRepository {
+  Future<List<Map<String, dynamic>>>? fetchData() async {
+    final response = listOfMovies;
 
-    return moviesModelFromJson(response);
+    return response;
+  }
+
+  Future<List<Map<String, dynamic>>>? updateData(
+      {required int index, required bool watchList}) async {
+    final response = listOfMovies;
+    if (response[index]['watchlist'] == true) {
+      response[index]['watchlist'] = false;
+    } else {
+      response[index]['watchlist'] = true;
+    }
+    return response;
+  }
+
+  Future<List<Map<String, dynamic>>>? sortData({required String sortby}) async {
+    final response = listOfMovies;
+    if (sortby == "title") {
+      response.sort((a, b) => a['title'].compareTo(b['title']));
+    } else {
+      response.sort((a, b) => a['released_date'].compareTo(b['released_date']));
+    }
+    return response;
   }
 }
